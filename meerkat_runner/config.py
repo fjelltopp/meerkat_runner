49370269eecf24f9188681c33c53954a1ad91415
Ubuntu <ubuntu@ip-10-0-0-141.eu-west-1.compute.internal>
2017-09-15 13:10:21 +0000

    Get task definitions in YAML instad of JSON

--- meerkat_runner/config.py
+++ meerkat_runner/config.py
@@ -14,9 +14,9 @@ if new_url:
     BROKER_URL = new_url
     CELERY_RESULT_BACKEND = new_url
 
-CELERY_TASK_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'yaml'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_ACCEPT_CONTENT = ['json']
+CELERY_ACCEPT_CONTENT = ['json','yaml']
 CELERY_IMPORTS = ('meerkat_abacus.tasks', 'api_background.export_data')
 CELERY_ENABLE_UTC = True
 #CELERYD_MAX_TASKS_PER_CHILD = 1  # To help with memory constraints
